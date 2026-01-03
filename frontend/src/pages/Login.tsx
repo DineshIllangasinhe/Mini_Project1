dab50263dc4002c8d28c3e6c19e9aa310d021eaf
Avishka Dinesh <avishka.dinesh12345@gmail.com>
2026-01-03 16:17:56 +0530

    changing login page ui

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -41,7 +41,7 @@ export default function Login() {
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
+            sign in to your account
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm space-y-6">
